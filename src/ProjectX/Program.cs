89fae548528a669161eaa69dafe06d4cea79e154
braunk <braunk@ZRD-WD-32196>
2017-01-16 16:49:10 +0100

    Projektdateien hinzufügen.

---
+++ src/ProjectX/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectX
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Test project
+        }
+    }
+}
