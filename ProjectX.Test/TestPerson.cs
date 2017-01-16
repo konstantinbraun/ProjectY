17b1dcc889b7a9d759a325a966af3282f07bee69
braunk <braunk@ZRD-WD-32196>
2017-01-16 17:27:22 +0100

    Add new test

---
+++ ProjectX.Test/TestPerson.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectX.Test
+{
+    [TestClass]
+    public class TestPerson
+    {
+        [TestMethod]
+        public void TestPerson_TestCunstructor_TheNameShouldBeTheSame()
+        {
+            Person o = new Person("Konstantin");
+            Assert.AreEqual(o.Name, "Konstantin");
+        }
+    }
+}
